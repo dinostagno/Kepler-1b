4caead4dab8408b8aa19032d4f9204875012e21f
Dino Stagno <85387888+dinostagno@users.noreply.github.com>
2021-10-21 01:06:56 -0300

    Update controller.js

--- controller.js
+++ controller.js
@@ -29,5 +29,23 @@ function ShowHelloMessage() {
     alert(name)
     document.getElementById("hellomessage").innerHTML = "Hello, " + name.value;
 }
-
-
+function loadService() {
+    console.log("index.html 7 | Get Covid Data");
+    const getCovidData = async () => {
+      console.log("index.html 10 | Processing...");
+      const request = await fetch('https://dinostagno-dev-ed.my.salesforce.com//services/apexrest/Accounts/0012E00002e4HURQA2', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer 00D2E000000mpVn!ARIAQLQmSGmFUxaLfwPmy0WQByzSCJGuw8NA2bJTlHmLr_Oq4P3mhBf_JpD6sIjAGt_KA0hlXVG0guAQKVTwooiSDNtrVZMA'
+        },
+        
+    });
+      const data = await request.json();
+      return data;
+    };
+    getCovidData().then(covidData => {
+      console.log("data", covidData);
+      //document.getElementById("total-cases").innerText = covidData.confirmed.value;
+    });
+}
