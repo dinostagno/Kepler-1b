8b9e5186f9aba334fd8d74fdb6edace0f120a2a1
dinostagno <dpaolostagno@gmail.com>
2021-10-25 20:36:42 -0300

    update

--- js/controller2.js
+++ js/controller2.js
@@ -1,10 +1,6 @@
 (async () => {
   const rawResponse = await fetch('https://login.salesforce.com/services/oauth2/authorize?client_id=3MVG9ZF4bs_.MKuhz4g8P5ekGrXZkNs0a2sba0CwY.6lhSXpZM4H.CM6okSQmxTiF0DMhGyMza2a_E4zfHbKn&redirect_uri=https://login.salesforce.com/services/oauth2/callback&response_type=code', {
-    method: 'GET',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
+    method: 'GET'
   });
   const content = await rawResponse.json();
 
